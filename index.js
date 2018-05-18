d1a3e1cdc247cf0e37c071bf14941ba5370170c3
Beatrice Mendoza <mendoza@nyu.edu>
2018-05-18 20:03:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   const cart = getCart();
-  const total = total();
+  const totalAmt = total();
   
   if (!cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
