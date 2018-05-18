c9958147cba01e6a6101c06e492f4e7106a39fc7
Beatrice Mendoza <mendoza@nyu.edu>
2018-05-18 19:53:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function removeFromCart(item) {
   
   for (let i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
-      cart.splice(, 1)
+      cart.splice(i, 1);
       return cart;
     }
   }
