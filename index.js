6526e13bbf408a5e5f41219f3095994eb3ba2272
Beatrice Mendoza <mendoza@nyu.edu>
2018-05-18 19:59:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,6 +70,8 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (!cardNumber) {
-    return 
+    return "Sorry, we don't have a credit card on file for you.";
+  } else {
+    
   }
 }
